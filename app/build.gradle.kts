8aa49a1d627ba945e35a17915811c820d6bd84e8
t2tato01 <t2tato01@students.oamk.fi>
2024-03-11 21:34:50 +0200

    started from part1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,12 @@ android {
 
 dependencies {
 
+    implementation ("androidx.navigation:navigation-compose:2.7.7")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -59,14 +65,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-
-    // Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    // Gson
-    implementation ("com.google.code.gson:gson:2.8.8")
-
+    implementation("androidx.benchmark:benchmark-macro:1.2.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/todo/viewmodel/TodoViewModel.kt
+++ app/src/main/java/com/example/todo/viewmodel/TodoViewModel.kt
@@ -1,5 +1,31 @@
 package com.example.todo.viewmodel
 
-class TodoViewModel {
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todo.model.Todo
+import com.example.todo.model.TodosApi
+import kotlinx.coroutines.launch
 
+class TodoViewModel : ViewModel() {
+    var todos = mutableStateListOf<Todo>()
+        private set
+
+    init {
+        getTodosList()
+    }
+
+    private fun getTodosList() {
+        viewModelScope.launch {
+            var todosApi: TodosApi? = null
+            try {
+                todosApi = TodosApi!!.getInstance()
+                todos.clear()
+                todos.addAll(todosApi.getTodos())
+            }catch (e: Exception) {
+                Log.d("TODOVIEWMODEL", e.message.toString())
+            }
+        }
+    }
 }
